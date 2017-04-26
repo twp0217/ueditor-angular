c11eb4c59af0cb49ee96e312b89251a27dd0908c
twp0217 <tu.wenpan@qq.com>
2017-04-26 22:58:49 +0800

    更新demo

---
+++ demo/js/controller.js
@@ -0,0 +1,20 @@
+var app = angular.module('app');
+app.controller('demo', ['$scope', function ($scope) {
+    $scope.onReady = function ($ueditor) {
+        console.log("onReady");
+        console.log($ueditor);
+    };
+
+    $scope.onContentChange = function ($content) {
+        console.log("onContentChange");
+        console.log($content);
+    }
+
+    $scope.setting = {
+        toolbars: [
+            ['fullscreen', 'source', 'undo', 'redo', 'bold']
+        ],
+        autoHeightEnabled: true,
+        autoFloatEnabled: true
+    };
+}]);
