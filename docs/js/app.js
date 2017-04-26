33ebed1ddcb0d0335cd4c6cb0ee49f4e688fb5ff
twp0217 <tu.wenpan@qq.com>
2017-04-26 21:20:03 +0800

    docs

---
+++ docs/js/app.js
@@ -0,0 +1,18 @@
+var app = angular.module('app', [
+    'ui.router',
+    'ueditor-angular'
+]);
+
+app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('demo', {
+            url: '/demo',
+            templateUrl: 'views/demo.html'
+        })
+        .state('documentation', {
+            url: '/documentation',
+            templateUrl: 'views/documentation.html'
+        });
+        
+    $urlRouterProvider.otherwise('/documentation');
+}]);
